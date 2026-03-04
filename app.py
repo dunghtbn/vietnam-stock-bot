b5779cb07aa08983b1ddf1c0e904b4b1228caa3e
dunghtbn <dunghtbn@gmail.com>
2026-03-04 10:46:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def load_data(symbol, timeframe):
         resolution = '1W' if timeframe == 'Tuần' else '1D'
         
         # Cú pháp khởi tạo đối tượng lấy dữ liệu của thế hệ 3 (sử dụng nguồn TCBS)
-        stock = Vnstock().stock(symbol=symbol, source='VND')
+        stock = Vnstock().stock(symbol=symbol, source='KBS')
         df = stock.quote.history(start=start_date, end=end_date, interval=resolution)
         
         if df is None or df.empty:
@@ -220,6 +220,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
